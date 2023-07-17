6c75877c31c20a2740e3e3b9bde3ff40772a699b
UBDJ <icek314@gmail.com>
2023-07-17 13:14:30 +0900

    UB20230717

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UB20230717
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
